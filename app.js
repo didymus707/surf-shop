163579f25752996d2115b7f7ebbdc8d07331e473
Adewale Orotayo <didymus7007@gmail.com>
2021-07-05 19:38:29 +0100

    Added user routes

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ var logger = require('morgan');
 var index = require('./routes/index');
 var posts = require('./routes/posts');
 var reviews = require('./routes/reviews');
-var users = require('./routes/users');
 
 var app = express();
 
@@ -24,7 +23,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/posts', posts);
 app.use('/posts/:id/reviews', reviews);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
